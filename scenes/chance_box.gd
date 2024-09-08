da72be684bd60a530e90799fa025b988e89a0a9c
Deuce Black <deuce2black@gmail.com>
2024-09-08 18:15:56 -0500

    added sprite mapping to internal tree tiles

--- scenes/chance_box.gd
+++ scenes/chance_box.gd
@@ -1,6 +1,34 @@
-class_name ChanceBox extends Sprite2D
+class_name ChanceBox extends Node2D
+
+var rng = RandomNumberGenerator.new()
+
+const NORMAL_ATLAS_REGIONS := [
+	Rect2(0, 0, 32, 32),
+	Rect2(32, 0, 32, 32),
+	Rect2(0, 32, 32, 32),
+	Rect2(160, 32, 32, 32),
+]
+const DECENT_HOLD_ATLAS_REGION := Rect2(192, 0, 32, 32)
+const GREAT_HOLD_ATLAS_REGION := Rect2(192, 32, 32, 32)
 
 var hold_strength := 0.0
+var normal_atlas_region_weights := PackedFloat32Array([1.0, 0.1, 0.1, 0.1])
 
-func _ready() -> void:
-	modulate = Color(0, hold_strength / 10.0, 0)
+func get_rect() -> Rect2:
+	return $BaseTreeSprite.get_rect()
+
+func set_atlas_regions(atlas: Texture2D) -> void:
+	var base_atlas_region := AtlasTexture.new()
+	base_atlas_region.atlas = atlas
+	base_atlas_region.region = NORMAL_ATLAS_REGIONS[rng.rand_weighted(normal_atlas_region_weights)]
+	$BaseTreeSprite.texture = base_atlas_region
+	
+	var hold_atlas_region := AtlasTexture.new()
+	hold_atlas_region.atlas = atlas
+	if hold_strength >= 9:
+		hold_atlas_region.region = GREAT_HOLD_ATLAS_REGION
+	elif hold_strength >= 7:
+		hold_atlas_region.region = DECENT_HOLD_ATLAS_REGION
+	else:
+		$HoldSprite.visible = false
+	$HoldSprite.texture = hold_atlas_region
--- scripts/tree_hold_mapper.gd
+++ scripts/tree_hold_mapper.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var rng = RandomNumberGenerator.new()
 
-const BOXES_PER_ROW := 10
+const BOXES_PER_ROW := 12
 const NUM_ROWS := 5
 
 var start_x := 0
@@ -12,9 +12,8 @@ var weights := PackedFloat32Array([1.0, 0.8, 0.7, 0.5])
 var grid = []
 
 func _ready() -> void:
-	start_y = get_viewport_rect().size.y
 	var chance_box = preload("res://scenes/chance_box.tscn")
-	# 3x sequential for loop required to first set base hold strengths, then place good holds, then place decent holds around those
+	var atlas_texture = load("res://assets/tree_sprite_sheet.png")
 	for i in BOXES_PER_ROW:
 		grid.append([])
 		for j in NUM_ROWS:
@@ -23,7 +22,10 @@ func _ready() -> void:
 			var rect: Rect2 = instance.get_rect()
 			# alter the overall start positions based on the box size when looking at the first box 
 			if i == 0 && j == 0:
+				var vp_rect = get_viewport_rect().size
+				start_x = (vp_rect.x / 2) - (rect.size.x * BOXES_PER_ROW / 2)
 				start_x += rect.size.x / 2
+				start_y = vp_rect.y
 				start_y -= rect.size.y / 2
 			instance.global_position = Vector2(start_x + (i * rect.size.x), start_y - (j * rect.size.y))
 			var good_hold_chance = randf()
@@ -35,6 +37,7 @@ func _ready() -> void:
 					instance.hold_strength = 7
 			else:
 				instance.hold_strength = strength
+			instance.set_atlas_regions(atlas_texture)
 			add_child(instance)
 			grid.append(instance)
 	
