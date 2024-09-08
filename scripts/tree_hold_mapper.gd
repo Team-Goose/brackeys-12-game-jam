0aec250674f399c0a360940c5204d71e25a332d2
Deuce Black <deuce2black@gmail.com>
2024-09-08 14:38:08 -0500

    added totally random grid generation

---
+++ scripts/tree_hold_mapper.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const BOXES_PER_ROW := 10
+const NUM_ROWS := 5
+
+var grid = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in NUM_ROWS:
+		grid.append([])
+		for j in BOXES_PER_ROW:
+			grid[i].append(randi_range(0, 10))
+	print(grid)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
