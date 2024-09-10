ee99647830ad2f10a6a2ddb77f08a89077f1fae5
Deuce Black <deuce2black@gmail.com>
2024-09-09 23:43:57 -0500

    oops should be using unique name references when possible

--- game.gd
+++ game.gd
@@ -1,12 +1,12 @@
 extends Node
 
 func _ready() -> void:
-	$TreeHoldMapper.visible = false
+	%TreeHoldMapper.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("esc"):
-		$TreeHoldMapper.visible = false
-		$MainMenu.visible = true
+		%TreeHoldMapper.visible = false
+		%MainMenu.visible = true
 
 func _on_cell_l_clicked(cell: TreeCell):
 	# TODO - propagate left click back down to player with proper functionality
@@ -21,8 +21,8 @@ func _on_cell_r_clicked(cell: TreeCell):
 		%TreeHoldMapper.add_tree_row()
 
 func _on_main_menu_play_pressed() -> void:
-	$MainMenu.visible = false
-	$TreeHoldMapper.visible = true
+	%MainMenu.visible = false
+	%TreeHoldMapper.visible = true
 
 func _on_main_menu_options_pressed() -> void:
 	print("options pressed") # TODO
