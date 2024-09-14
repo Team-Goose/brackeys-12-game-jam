4f12cdb4aa1cd9be629dda6b0f5a982dcf874a50
Deuce Black <deuce2black@gmail.com>
2024-09-14 02:22:50 -0500

    initial leaderboard request populating

--- scripts/leaderboard.gd
+++ scripts/leaderboard.gd
@@ -12,4 +12,29 @@ func _on_request_completed(result, response_code, headers, body) -> void:
 		%LeaderboardVBox.remove_child(child)
 	%LoadingSpinnerBox.visible = false
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json)
+	for obj in json:
+		var un_label := Label.new()
+		var score_label := Label.new()
+		var day_label := Label.new()
+		var hbox := HBoxContainer.new()
+		var color_rect := ColorRect.new()
+		un_label.text = obj['username']
+		un_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		un_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		score_label.text = obj['score']
+		score_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		score_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		day_label.text = obj['day']
+		day_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		day_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		color_rect.color = Color.WHITE
+		color_rect.custom_minimum_size = Vector2(852, 5)
+		hbox.add_child(un_label)
+		hbox.add_child(score_label)
+		hbox.add_child(day_label)
+		%LeaderboardVBox.add_child(hbox)
+		%LeaderboardVBox.add_child(color_rect)
+
+
+func _on_return_to_title_button_pressed() -> void:
+	queue_free()
